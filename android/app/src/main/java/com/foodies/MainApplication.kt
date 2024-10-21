6abf239659785a6260a55e7f9c5fbbab021a4982
swadexpress <muhammadkawsarkhan@gmail.com>
2024-10-21 20:34:55 +0600

    up

--- android/app/src/main/java/com/foodies/MainApplication.kt
+++ android/app/src/main/java/com/foodies/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.orbitplug
+package com.foodies
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/FoodiesTests/FoodiesTests.m
+++ ios/FoodiesTests/FoodiesTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface orbitplugTests : XCTestCase
+@interface FoodiesTests : XCTestCase
 
 @end
 
-@implementation orbitplugTests
+@implementation FoodiesTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
